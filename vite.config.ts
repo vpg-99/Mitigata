717a09e9494aae82190966a1edd04915084ee87c
vpg-99 <vishnupriyagupta93@gmail.com>
2025-11-17 20:04:42 +0530

    Deploying to Github Actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Mitigata/', // Replace with your GitHub repository name
   server: {
     port: 3000,
     open: true
